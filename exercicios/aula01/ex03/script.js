cde4c4031a38e7ffe9fe1b28dfba05d49318d650
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-03-15 18:42:02 -0300

    pequenas alteraçoes

--- exercicios/aula01/ex03/script.js
+++ exercicios/aula01/ex03/script.js
@@ -1,4 +1,5 @@
 function clicar(){
-    var nome = window.prompt('Digite o seu nome')  
-    document.write(`Olá, ${nome}! É um grande prazer te conhecer! \u{1F596}`)
+    let nome = window.prompt('Digite o seu nome')  
+    let res = window.document.getElementById('resultado')
+    res.innerHTML = `<p>Olá, ${nome}! É um grande prazer te conhecer! \u{1F596}</p>`
 }
