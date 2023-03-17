da7d60c60d1b9eebdc4677768d137d01913dffed
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-03-16 21:26:25 -0300

    Update script08.js

--- exercicios/aula01/ex08/script08.js
+++ exercicios/aula01/ex08/script08.js
@@ -2,6 +2,6 @@ function contador(){
     var cont = window.document.getElementById('cont')
     var res = window.document.getElementById('resultado')
     cont.addEventListener('click', clicar)
-    
+    var res = cont + 1
     cont.innerHTML = `O contador está com ${cont} cliques.`
 }
